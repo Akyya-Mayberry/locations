c20d524524804c23c15d22276afc4e6eb8fc1249
hollywoodno <hollywoodno@gmail.com>
2018-10-02 19:36:39 -0700

    add basic fullpage map

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,28 @@
 import * as React from 'react';
 import './App.css';
+import Map from './components/Map'
 
 import logo from './logo.svg';
 
 class App extends React.Component {
-  public render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    public render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h1 className="App-title">Welcome to React</h1>
+                </header>
+                <div className="App-intro">
+                    <Map
+                        googleMapURL={`https://maps.googleapis.com/maps/api/js?key=AIzaSyDzYuNeeymGaSyKN6z1wtIpgZDpgq-ckTc&v=3.exp&libraries=geometry,drawing,places`}
+                        loadingElement={<div style={{ height: `100%` }} />}
+                        containerElement={<div style={{ height: `400px` }} />}
+                        mapElement={<div style={{ height: `100%` }} />}
+                    />
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
