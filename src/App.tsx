0c84ea71e3d179bb908a45991d759da77beb7d08
hollywoodno <hollywoodno@gmail.com>
2018-10-02 19:43:24 -0700

    update tslint rules

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import * as React from 'react';
 import './App.css';
-import Map from './components/Map'
+import Map from './components/Map';
 
 import logo from './logo.svg';
 
 class App extends React.Component {
     public render() {
         return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h1 className="App-title">Welcome to React</h1>
+            <div className='App'>
+                <header className='App-header'>
+                    <img src={logo} className='App-logo' alt='logo' />
+                    <h1 className='App-title'>Welcome to React</h1>
                 </header>
-                <div className="App-intro">
+                <div className='App-intro'>
                     <Map
                         googleMapURL={`https://maps.googleapis.com/maps/api/js?key=AIzaSyDzYuNeeymGaSyKN6z1wtIpgZDpgq-ckTc&v=3.exp&libraries=geometry,drawing,places`}
                         loadingElement={<div style={{ height: `100%` }} />}
